e6a01e6617ea6655589e7a8cce0729c7756d218d
ChangCarlos <132376682+ChangCarlos@users.noreply.github.com>
2023-12-01 14:18:54 -0300

    Alteração no caminho da imagem

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function alterarContexto(contexto) {
         contexto.classList.remove('active');
     });
     html.setAttribute('data-contexto', contexto);
-    banner.setAttribute('src', `/imagens/${contexto}.png`);
+    banner.setAttribute('src', `./imagens/${contexto}.png`);
     switch (contexto) {
         case "foco":
             titulo.innerHTML = `
